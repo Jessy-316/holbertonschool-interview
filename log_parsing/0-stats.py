1ea9a1a507625a6a9d4e73404e21962fec1f18a4
Francisco Baez <jessiebaez316@outlook.com>
2025-04-26 13:53:03 -0400

    task done

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,46 +1,44 @@
 #!/usr/bin/python3
-"""Statistics module."""
+"""Statistics module for analyzing logs."""
 import sys
 
-"""Dictionary to store counts of status codes"""
-stats = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
 
-"""List to store file sizes"""
-sizes = [0]
-
-
-def print_stats():
-    """Print statistics."""
-    print('File size: {}'.format(sum(sizes)))
+def print_stats(stats, total_size):
+    """Print the statistics for status codes and total file size."""
+    print('File size: {}'.format(total_size))
     for s_code, count in sorted(stats.items()):
         if count:
             print('{}: {}'.format(s_code, count))
 
 
-"""Main program"""
-try:
-    for i, line in enumerate(sys.stdin, start=1):
-        matches = line.rstrip().split()
-        try:
-            status_code = matches[-2]
-            file_size = matches[-1]
-            if status_code in stats.keys():
-                stats[status_code] += 1
-            sizes.append(int(file_size))
-        except Exception:
-            pass
-        if i % 10 == 0:
-            print_stats()
-    print_stats()
-except KeyboardInterrupt:
-    print_stats()
-    raise
+if __name__ == "__main__":
+    total_size = 0
+    count = 0
+    stats = {
+        '200': 0, '301': 0, '400': 0, '401': 0,
+        '403': 0, '404': 0, '405': 0, '500': 0
+    }
+
+    try:
+        for line in sys.stdin:
+            count += 1
+            try:
+                parts = line.split()
+                if len(parts) > 2:
+                    status_code = parts[-2]
+                    file_size = int(parts[-1])
+                    
+                    if status_code in stats:
+                        stats[status_code] += 1
+                    total_size += file_size
+            except (IndexError, ValueError):
+                pass
+
+            if count % 10 == 0:
+                print_stats(stats, total_size)
+                
+        print_stats(stats, total_size)
+                
+    except KeyboardInterrupt:
+        print_stats(stats, total_size)
+        raise
